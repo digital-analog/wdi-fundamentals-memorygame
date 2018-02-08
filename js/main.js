608d4db7261071db16a1ac12404476b9d920d094
Digi Barcelona <digi.barcelona@disney.com>
2018-02-08 12:02:45 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-//Create variables to store each of the 4 cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//Create an array to store all cards
+var cards = ["queen", "queen", "king","king"];
 
+// create array to hold cards that are in play
+var cardsInPlay = [];
+
+// Create variable for first card user flipped
+var cardOne = cards[0];
+
+// Add this card to cardsInPlay array
+cardsInPlay.push(cardOne);
+
+// Display card user flipped on the console
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+
+// Create variable for second card user flipped
+var cardTwo = cards[2];
+
+// Add this card to cardsInPlay array
+cardsInPlay.push(cardTwo);
+
+// Display card user flipped on the console
+console.log("User flipped " + cardTwo);
+
+// Check if two cards have been played
+if (cardsInPlay.length === 2) {
+
+  //Check if two cards played match and display alert message to user
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+       alert("Sorry, try again.");
+     }
+}
