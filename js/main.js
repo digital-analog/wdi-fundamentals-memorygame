2f79180699f4ca51558e3790b6317ccbc0cdadad
Digi Barcelona <digi.barcelona@disney.com>
2018-02-08 13:50:56 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,31 +4,34 @@ var cards = ["queen", "queen", "king","king"];
 // create array to hold cards that are in play
 var cardsInPlay = [];
 
-// Create variable for first card user flipped
-var cardOne = cards[0];
-
-// Add this card to cardsInPlay array
-cardsInPlay.push(cardOne);
-
-// Display card user flipped on the console
-console.log("User flipped " + cardOne);
-
-// Create variable for second card user flipped
-var cardTwo = cards[2];
-
-// Add this card to cardsInPlay array
-cardsInPlay.push(cardTwo);
-
-// Display card user flipped on the console
-console.log("User flipped " + cardTwo);
-
-// Check if two cards have been played
-if (cardsInPlay.length === 2) {
+// Create a function to check for a match
+var checkForMatch = function () {
 
   //Check if two cards played match and display alert message to user
-    if (cardsInPlay[0] === cardsInPlay[1]) {
-      alert("You found a match!");
-    } else {
-       alert("Sorry, try again.");
-     }
-}
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+  } else {
+     alert("Sorry, try again.");
+   }
+  };
+
+// Create a function for when user flips a card
+var flipCard = function (cardId) {
+
+  //Display in the console the card that user flipped
+  console.log("User flipped " + cards[cardId]);
+
+  //Add the card user flipped to the array of cards in play
+  cardsInPlay.push(cards[cardId]);
+
+  // Check to see if two cards were played
+  if (cardsInPlay.length === 2) {
+    // If so, call the checkForMatch function
+    checkForMatch();
+  }
+};
+
+// Call the flipCard function, passing in 0 as an argument
+flipCard(0);
+//Call the flipCard function, passing in 2 as an argument
+flipCard(2);
