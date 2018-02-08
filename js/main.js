8540a5a3db4db394c9f5750a8097a07ec431b5b0
Digi Barcelona <digi.barcelona@disney.com>
2018-02-08 10:47:18 -0800

    created game logic

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Create variables to store each of the 4 cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
